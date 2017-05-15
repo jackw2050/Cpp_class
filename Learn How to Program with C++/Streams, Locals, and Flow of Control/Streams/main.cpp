27a46c274e69780bb45e51b2061c43cbafbbb976
jackw2050 <jack2050@gmail.com>
2017-05-15 11:43:57 -0500

    initial

---
+++ Learn How to Program with C++/Streams, Locals, and Flow of Control/Streams/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: jackwalker
+ *
+ * Created on May 15, 2017, 11:41 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+        std::cout << "Hello World!!!" <<std::endl;    
+    std::cout << 2+2;
+    std::cout <<std::endl; 
+    // same as 
+    
+      std::cout << "Hello World!!!" <<std::endl<< 2+2;
+    return 0;
+}
+
